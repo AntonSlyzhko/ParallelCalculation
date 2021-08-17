a64ab3995bb96349cebd42f5b16bcc7cd1673258
Anton Slyzhko <slyzhko2001@gmail.com>
2021-08-18 01:03:33 +0300

    Creating close request functionality

--- src/main/java/com/mycompany/project/App.java
+++ src/main/java/com/mycompany/project/App.java
@@ -2,30 +2,37 @@ package com.mycompany.project;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import java.util.Optional;
 
 public class App extends Application {
 
-    private static Scene scene;
-
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("primary.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 834, 467);
+        stage.setTitle("Author: Anton Slyzhko");
         stage.setScene(scene);
         stage.show();
+        stage.setOnCloseRequest(e ->{
+            e.consume();
+            closeRequest(stage);
+        });
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+    private void closeRequest(Stage stage){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Exit agreement");
+        alert.setHeaderText("Do you really want to quit?");
+        alert.setContentText(null);
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.orElse(null) == ButtonType.OK){
+            stage.close();
+        }
     }
 
     public static void main(String[] args) {
